6f5011b070a581a463bca3a12d144fc6cb051655
Paul Cothenet <pcothenet@gmail.com>
2014-08-14 14:10:37 -0700

    Fixing header + moving to .js files

---
+++ blog/wp-content/themes/webflow/header.js
@@ -0,0 +1,18 @@
+document.write('\
+\
+<div class="w-nav header" id="top" data-collapse="medium" data-animation="default" data-duration="400" data-contain="1">\
+	<a class="w-nav-brand brand-logo" href="index.html">\
+		<img src="../images/madkudu_rectangle.png" alt="53cc00fa46631a7b2828fd06_madkudu_rectangle.png">\
+	</a>\
+	<div class="w-clearfix header-contact">\
+		<div class="header-text">contact@madkudu.com /&nbsp;(888) 407-8572</div>\
+	</div>\
+	<div class="w-container navigation-container">\
+		<nav class="w-nav-menu nav-menu" role="navigation">\
+		<a class="w-nav-link nav-link" href="index.html">Home</a>\
+		<a class="w-nav-link nav-link" href="/blog">Blog</a>\
+		</nav>\
+	</div>\
+</div>\
+\
+');
---
+++ blog/wp-content/themes/webflow/mailchimp.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+<div class="w-form mc-wrapper">\
+\
+	<form class="mc_embed_signup" id="wf-form-mc-embedded-subscribe-form" name="wf-form-mc-embedded-subscribe-form" data-name="mc-embedded-subscribe-form" action="//madkudu.us8.list-manage.com/subscribe/post?u=50d10dbb43453c101e00a5f6f&amp;amp;id=2562f30397">\
+\
+		<label class="field-label" for="mc-email">Subscribe to our marketing tips</label>\
+\
+		<input class="w-input" id="mc-email" type="email" placeholder="Enter your email address" name="email" data-name="EMAIL" required="required">\
+\
+		<input class="w-button" type="submit" value="Subscribe" data-wait="Please wait...">\
+	</form>\
+\
+	<div class="w-form-done">\
+		<p>Thank you! Your submission has been received!</p>\
+	</div>\
+	<div class="w-form-fail">\
+		<p>Oops! Something went wrong while submitting the form :(</p>\
+	</div>\
+</div>\
+\
+');
