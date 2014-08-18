e688c96c009aedf909ee365d8e8f2f211b2a13b5
Paul Cothenet <pcothenet@gmail.com>
2014-08-18 16:54:52 -0700

    Sitemap and minor tracking issues

--- blog/wp-content/themes/ultimatum/header.php
+++ blog/wp-content/themes/ultimatum/header.php
@@ -33,33 +33,6 @@
 	<?php wp_head(); ?>
 	<?php if(get_ultimatum_option('scripts', 'head_scripts')){ echo stripslashes(get_ultimatum_option('scripts', 'head_scripts'));}echo "\n"; ?>
 	<?php do_action('ultimatum_before_head_close');?>	 
-
-	<!--Begin Mixpanel-->
-	<script type="text/javascript">
-		(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
-			for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
-				mixpanel.init("bba364b1e340c1e0f62357fb558ff21a");
-		</script>
-		<!--End Mixpanel-->
-		<script>
-			dataLayer = [
-			{
-				'event': 'Blog'
-			}
-			];
-		</script>
-		<!-- Google Tag Manager -->
-		<noscript>
-			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MNF2ST" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-		</noscript>
-		<script>
-			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-MNF2ST');
-	</script>
-	<!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?>>
 	<?php do_action('ultimatum_after_body_open');?>
---
+++ blog/wp-content/themes/webflow/script.js
@@ -0,0 +1,31 @@
+// Begin Mixpanel 
+(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+	for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+		mixpanel.init("bba364b1e340c1e0f62357fb558ff21a");
+// End Mixpanel
+
+// GTM dataLayer
+dataLayer = [
+{
+	'event': 'Blog'
+}
+];
+
+// Google Tag Manager
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MNF2ST');
+<!-- End Google Tag Manager -->
+
+$(function() {
+	$('#wf-form-mc-embedded-subscribe-form').on('submit', function(e){
+		e.preventDefault();
+		var val = $("#mc-email").val();
+		dataLayer.push({
+			'event': 'Newsletter sign-up',
+			'user_email': val
+		});
+	});
+});
