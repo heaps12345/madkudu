f2ff0c071f0acde22bdea827fccb72f9231b6522
Paul Cothenet <pcothenet@gmail.com>
2014-08-22 22:25:42 -0700

    Switching to segment.io

--- blog/wp-content/themes/webflow/script.js
+++ blog/wp-content/themes/webflow/script.js
@@ -1,39 +1,33 @@
-// Begin Mixpanel 
-(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
-	for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
-		mixpanel.init("bba364b1e340c1e0f62357fb558ff21a");
-// End Mixpanel
-
-// Begin Google Tag Manager
-dataLayer = [
-{
-	'event': 'Website'
-}
-];
-
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MNF2ST');
-// End Google Tag Manager
-
 // Begin Segment
 window.analytics=window.analytics||[],window.analytics.methods=["identify","group","track","page","pageview","alias","ready","on","once","off","trackLink","trackForm","trackClick","trackSubmit"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var key=window.analytics.methods[i];window.analytics[key]=window.analytics.factory(key)}window.analytics.load=function(t){if(!document.getElementById("analytics-js")){var a=document.createElement("script");a.type="text/javascript",a.id="analytics-js",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.io/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)}},window.analytics.SNIPPET_VERSION="2.0.9",
 window.analytics.load("tcuowrwxhc");
-window.analytics.page();
+window.analytics.page('Blog');
 // End Segment
 
 $(function() {
 	$('#mc-embedded-subscribe-form').on('submit', function(e){
 		//e.preventDefault();
 		var val = $("#mce-EMAIL").val();
+		
+		analytics.track('Newsletter sign-up', {
+  		email: val
+		});
+
+		analytics.alias(val);
+
+		analytics.identify(val, {
+		  email: val
+		});
+
+		/*
 		dataLayer.push({
 			'event': 'Newsletter sign-up',
 			'user_email': val
 		});
+		*/
 	});
 
+	/*
 	$('#mc-embedded-subscribe-popup').on('submit', function(e){
 		//e.preventDefault();
 		console.log("Submit")
@@ -43,4 +37,5 @@ $(function() {
 			'user_email': val
 		});
 	});
+	*/
 });
