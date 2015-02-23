b4c963b7a50eedd203fb320e5470707efc8fa17d
Paul Cothenet <pcothenet@gmail.com>
2015-02-23 09:57:20 -0800

    More robust header

--- js/header.js
+++ js/header.js
@@ -1,34 +1,28 @@
 /* globals $, document */
 
 $(document).ready(function (){
-
 	'use strict';
 
-	var update_class_nav_bar = function(click_selector) {
-		$('#navbar_what').removeClass('active');
-		$('#navbar_how').removeClass('active');
-		$('#navbar_why').removeClass('active');
-		$(click_selector).addClass('active');
-	};
-
-	var activate = function(click_selector, goto_selector, dont_format) {
-			$(click_selector).click(function(e) {
-				e.preventDefault();
-				if (!dont_format) {
-					update_class_nav_bar(click_selector);
-				}
-				$('html, body').animate({
-					scrollTop: $(goto_selector).offset().top - 20
-				}, 1500);
-			});
-	};
+	var scroll_offset = 20;
 
 	if (window.location.pathname === '/') {
 		$('.navbar-nav--madkudu').show();
-		activate('#navbar_what', '#section-explainer');
-		activate('#navbar_how', '#section-benefits');
-		activate('#navbar_why', '#section-why-madkudu');
-		activate('#navbar_invite', '#section-signup', true);
 	}
+
+	$('body').scrollspy({ target:'.navbar--madkudu', offset: scroll_offset});
+
+	// Smooth navigation
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top - scroll_offset
+				}, 1500);
+				return false;
+			}
+		}
+	});
 
 });
