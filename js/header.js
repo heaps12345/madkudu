a62ade005b82fcd3088b21b505c67e9c97948fff
Paul Cothenet <pcothenet@gmail.com>
2015-02-23 09:14:08 -0800

    Do not display header links on the other pages

--- js/header.js
+++ js/header.js
@@ -4,8 +4,6 @@ $(document).ready(function (){
 
 	'use strict';
 
-	$('.navbar-nav--madkudu').show();
-
 	var update_class_nav_bar = function(click_selector) {
 		$('#navbar_what').removeClass('active');
 		$('#navbar_how').removeClass('active');
@@ -14,18 +12,23 @@ $(document).ready(function (){
 	};
 
 	var activate = function(click_selector, goto_selector, dont_format) {
-		$(click_selector).click(function() {
-			if (!dont_format) {
-				update_class_nav_bar(click_selector);
-			}
-			$('html, body').animate({
-				scrollTop: $(goto_selector).offset().top - 20
-			}, 1500);
-		});
+			$(click_selector).click(function(e) {
+				e.preventDefault();
+				if (!dont_format) {
+					update_class_nav_bar(click_selector);
+				}
+				$('html, body').animate({
+					scrollTop: $(goto_selector).offset().top - 20
+				}, 1500);
+			});
 	};
 
-	activate('#navbar_what', '#section-explainer');
-	activate('#navbar_how', '#section-benefits');
-	activate('#navbar_why', '#section-why-madkudu');
-	activate('#navbar_invite', '.section-signup', true);
+	if (window.location.pathname === '/') {
+		$('.navbar-nav--madkudu').show();
+		activate('#navbar_what', '#section-explainer');
+		activate('#navbar_how', '#section-benefits');
+		activate('#navbar_why', '#section-why-madkudu');
+		activate('#navbar_invite', '.section-signup', true);
+	}
+
 });
