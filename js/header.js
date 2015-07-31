29ef8c954aca7b7e55507c4a054026b732369325
Sam Levan <sam@madkudu.com>
2015-07-31 10:54:41 -0600

    add hotjar

--- js/header.js
+++ js/header.js
@@ -60,6 +60,16 @@ $(document).ready(function (){
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(se, s);
 	// <!-- end SnapEngage code -->
 
+	// <!-- Hotjar Tracking Code for www.madkudu.com -->
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:57586,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+
 
 
 });
