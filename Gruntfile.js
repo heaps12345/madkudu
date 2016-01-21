f4ce7dd285c7d8f206355a0a1c3c0769680f3d68
Paul Cothenet <pcothenet@gmail.com>
2016-01-21 10:35:27 -0800

    Remove /dist from git

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,74 +1,11 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-	// would be better to use this package
-	// require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt);
 
-	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// Task configuration.
-		autoprefixer: {
-			dest: {
-				src: ['css/*.css']
-			}
-		},
-		clean: {
-			build: {
-				src: ['new_version/','madkudu.webflow.zip']
-			}
-		},
-		copy: {
-			main: {
-				files: [{
-					expand: true,
-					cwd: './new_version/',
-					src: '**',
-					dest: '.'
-				}]
-			},
-			css: {
-				files: [{
-					expand: true,
-					cwd: './css/',
-					src: '**',
-					dest: './blog/wp-content/themes/webflow/css/'
-				}]
-			},
-			vendor: {
-				// Only had files that need to be compiled (e.g. bootstrap css)
-				// For precompiled stuff (jquery), use cdnjs instead
-				files: [
-				{
-					expand: true, cwd: 'bower_components/bootstrap/dist/js',
-					src: ['bootstrap.min.js'], dest: 'js'
-				},
-				{
-					expand: true, cwd: 'bower_components/jquery.inputmask/dist/',
-					src: ['jquery.inputmask.bundle.min.js'], dest: 'js'
-				},
-				{
-					expand: true, cwd: 'bower_components/underscore/',
-					src: ['underscore-min.js'], dest: 'js'
-				},
-				{
-					expand: true, cwd: 'bower_components/numeral/min/',
-					src: ['numeral.min.js'], dest: 'js'
-				},
-				{
-					expand: true, cwd: 'bower_components/q/',
-					src: ['q.js'], dest: 'js'
-				}
-				]
-			},
 
-		},
-		browserify: {
-			main: {
-				src: 'js/simulator/main.js',
-				dest: 'js/simulator/bundle.js'
-			}
-		},
 		ftpush: {
 			site: {
 				auth: {
@@ -76,7 +13,7 @@ module.exports = function(grunt) {
 					port: 21,
 					authKey: 'ftpkey'
 				},
-				src: '.',
+				src: './dist',
 				dest: '/public_html/',
 				exclusions: ['**/blog/**','**/.gitignore','**/.git/**','**/.grunt/**','**/node_modules/**','**/bower_components/**',
 				'**/.DS_Store', '**/Thumbs.db','**/.ftppass',
@@ -84,79 +21,10 @@ module.exports = function(grunt) {
 				keep: ['/public_html/blog/**','/public_html/blog/*'],
 				simple: true
 			},
-			blog: {
-				auth: {
-					host: 'server40.web-hosting.com',
-					port: 21,
-					authKey: 'ftpkey'
-				},
-				src: './blog/wp-content/themes/webflow',
-				dest: '/public_html/blog/wp-content/themes/webflow',
-				exclusions: ['**/.gitignore','**/.git/**','**/.DS_Store', '**/Thumbs.db','**/.ftppass','**/dev-*'],
-				simple: true
-			}
 		},
-		gitcommit: {
-			site: {
-				options: {
-					ignoreEmpty: 'true'
-				},
-				files: {
-					src: ['*.html','css/**','images/**']
-				}
-			}
-		},
-		jade: [{
-			expand: true,
-			cwd: 'jade/pages/',
-			src: ['*.jade'],
-			dest: '.',
-			ext: '.html'
-		}],
-		less: {
-			options: {
-				compress: true
-			},
-			layouts: {
-				files: [
-				{
-					'css/index.min.css': 'less/index.less'
-				}
-				]
-			}
-		},
-		unzip: {
-			catalog: {
-				src: 'madkudu.webflow.zip',
-				dest: './new_version/'
-			}
-		},
-		watch: {
-			client: {
-				files: ['less/**/*.less','js/**/*.js','jade/**/*.jade'],
-				tasks: ['build']
-			}
-		},
+
 	});
 
-	// These plugins provide necessary tasks.
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-jade');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-ftpush');
-	grunt.loadNpmTasks('grunt-git');
-	grunt.loadNpmTasks('grunt-newer');
-	grunt.loadNpmTasks('grunt-zip');
-	grunt.loadNpmTasks('grunt-browserify');
-
 	// Default task.
-	grunt.registerTask('default', ['build', 'watch']);
-	grunt.registerTask('build', ['copy:vendor','less','jade','browserify','autoprefixer']);
-	grunt.registerTask('unpack', ['unzip','copy:main','clean']);
-	grunt.registerTask('commit', ['gitcommit:site','gitpush:site']);
 	grunt.registerTask('push-site', ['ftpush:site']);
-	grunt.registerTask('push-blog', ['copy:css','ftpush:blog']);
 };
